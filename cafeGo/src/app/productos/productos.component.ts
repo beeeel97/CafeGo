be1cce8436e0fcd9251a36e99ebb299d50b40820
maribellopezromero <maribel.romero.miv@gmail.com>
2023-05-18 13:29:50 +0200

    Mostrar productos por categoria

--- cafeGo/src/app/productos/productos.component.ts
+++ cafeGo/src/app/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceProducto } from '../services/servicio-producto.service';
+import { Producto } from '../models/Producto';
 
 @Component({
   selector: 'app-productos',
@@ -12,26 +13,43 @@ export class ProductosComponent {
 
   constructor(private router:Router, private route:ActivatedRoute, private ServiceProducto: ServiceProducto) {
     this.IDUsuario = this.route.snapshot.queryParams['usuario'];
-  this.categoria.categoria = this.route.snapshot.queryParams['categoria'];
-  console.log("categoria en productos", this.categoria.categoria);
+    this.categoria = this.route.snapshot.queryParams['categoria'];
   this.obtenerProductoByCategoria();
     
    }
- 
 
+  //queryParams
    IDUsuario: number;
-   categoria:any={
-    categoria:""
-   }
+   categoria:string;
+
+   //guardar los productos que me vienen de la base de datos
+   productos: Producto[] = [];
+
+ 
 
    obtenerProductoByCategoria() {
     console.log("categoria en productos", this.categoria);
-    this.ServiceProducto.obtenerProductoByCategoria(this.categoria).subscribe();
+    this.ServiceProducto.obtenerProductoByCategoria(this.categoria).subscribe(data => {
+
+      console.log(data)
+
+       // Convertir las propiedades del objeto en un array
+       const dataArray = Object.values(data);
+       console.log(dataArray);
+
+        this.productos = dataArray.map((objeto:any) => {
+        return new Producto(
+          Number(objeto.IDProducto),
+          objeto.NombreProducto,
+          Number(objeto.Categoria),
+          Number(objeto.Frio),
+          Number(objeto.UnidadProducto),
+          Number(objeto.PrecioProducto),
+          objeto.DescripcionProducto
+        )});
+      
+      console.log( this.productos)
+    });
   }
-
-
-
-
-  
 
 }
