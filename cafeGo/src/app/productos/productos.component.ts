5a85ba6cb50db4b5ff34662c81005d0542af853c
maribellopezromero <maribel.romero.miv@gmail.com>
2023-05-23 19:14:42 +0200

    nada

--- cafeGo/src/app/productos/productos.component.ts
+++ cafeGo/src/app/productos/productos.component.ts
@@ -1,64 +1,3 @@
-// import { Component } from '@angular/core';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { ServiceProducto } from '../services/servicio-producto.service';
-// import { Producto } from '../models/Producto';
-
-// @Component({
-//   selector: 'app-productos',
-//   templateUrl: './productos.component.html',
-//   styleUrls: ['./productos.component.css']
-// })
-// export class ProductosComponent {
-
-
-//   constructor(private router:Router, private route:ActivatedRoute, private ServiceProducto: ServiceProducto) {
-//     this.IDUsuario = this.route.snapshot.queryParams['usuario'];
-//     this.categoria = this.route.snapshot.queryParams['categoria'];
-
-//     console.log("aqui",this.categoria)
-//      this.obtenerProductoByCategoria();
-    
-//    }
-
-//   //queryParams
-//    IDUsuario: number;
-//    categoria:string;
-
-//    //guardar los productos que me vienen de la base de datos
-//    productos: Producto[] = [];
-
- 
-
-//    obtenerProductoByCategoria() {
-//     console.log("categoria en productos", this.categoria);
-//     this.ServiceProducto.obtenerProductoByCategoria(this.categoria).subscribe(data => {
-
-//       console.log(data)
-
-//        // Convertir las propiedades del objeto en un array
-//        const dataArray = Object.values(data);
-//        console.log(dataArray);
-
-//         this.productos = dataArray.map((objeto:any) => {
-//         return new Producto(
-//           Number(objeto.IDProducto),
-//           objeto.NombreProducto,
-//           Number(objeto.Categoria),
-//           Number(objeto.Frio),
-//           Number(objeto.UnidadProducto),
-//           Number(objeto.PrecioProducto),
-//           objeto.DescripcionProducto
-//         )});
-      
-//       console.log( this.productos)
-//     });
-
-//   }
-
-
-
-// }
-
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ServiceProducto } from '../services/servicio-producto.service';
@@ -134,4 +73,3 @@ export class ProductosComponent implements OnInit {
 
 
 }
-
